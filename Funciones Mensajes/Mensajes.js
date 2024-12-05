2ca2f1650e1a519f3599cb446ad59006b37b0063
IskanderM08 <IskanderMercader2002@gmail.com>
2024-12-04 20:05:23 -0600

    Realización Funciones Mensajes

---
+++ Funciones Mensajes/Mensajes.js
@@ -0,0 +1,11 @@
+function displayGreeting1(msg) {
+    console.log(msg);
+}
+
+function displayGreeting2(msg1, msg2) {
+    console.log(msg1);
+    console.log(msg2);
+}
+
+displayGreeting1("hola");
+displayGreeting2("hola", "adios");
